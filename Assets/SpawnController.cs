af4073ef82d1b281b586f8047c4b234ee5587e2c
seongwon1579 <seongwon1579@gmail.com>
2022-12-22 13:31:52 +0900

    Dec 22

--- Assets/SpawnController.cs
+++ Assets/SpawnController.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System;
+using System.Linq;
+
+
 
 public class SpawnController : MonoBehaviour
 {
-    public Detectation[] enemy;
+    //public Object[] enemy;
+    public GameObject[] enemy;
+    List<GameObject> enemySub = new List<GameObject>();
 
     [SerializeField]
     private Vector3 spawnPos;
 
+    //private void Start()
+    //{
+    //    enemy = Resources.LoadAll("Prefab", typeof(GameObject));
+    //    foreach (var temp in enemy)
+    //    {
+    //        enemySub.Add(temp as GameObject);
+    //    }
+    //}
+
     private void Start()
     {
-
-        //enemy = Resources.Load<>;
-
+        enemy = Resources.LoadAll("Prefab", typeof(GameObject)).Cast<GameObject>().ToArray();
+        foreach(var temp in enemy)
+        {
+            enemySub.Add(temp);
+        }
     }
 
     public void Spawn()
     {
-        int randomIndex = Random.Range(0, enemy.Length-1);
-        
+        int randomIndex = Random.Range(0, enemy.Length - 1);
         Instantiate(enemy[randomIndex].gameObject, spawnPos, Quaternion.identity);
     }
 }
